877a07fa300ec51ef3299ff481034e49fdf44c24
Jair Zeferino Júnior <jair.zeferino@kleincode.com>
2021-03-05 17:54:35 -0300

    Fixed code generation outside the root directory

--- src/extension.ts
+++ src/extension.ts
@@ -18,83 +18,82 @@ export function activate(context: vscode.ExtensionContext) {
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('automagicCode.generate', () => {
-        var editor = vscode.window.activeTextEditor;
+        const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return; // No open text editor
-        }        
-        var config = require(editor.document.fileName);
-        var metadata = require(config.metadataJsonFile);
+        }
+        const fileDir = path.dirname(editor.document.fileName)
+        const config = require(editor.document.fileName);
+        const metadata = require(`${fileDir}\\${config.metadataJsonFile}`);
         config.templates.forEach(template => {
             mu.clearCache();
-            mu.root = path.dirname(template.mustacheFile)
+            mu.root = path.dirname(`${fileDir}\\${template.mustacheFile}`)
             mu.fileName = path.basename(template.mustacheFile)
-            mu.extension = path.extname(template.mustacheFile)        
-            
-            var result = "";            
-            mu.compileAndRender(mu.fileName, metadata)        
-            .on('data', function (data) {                        
-                result += data.toString();            
-            })
-            .on("end", function () {            
-                var contextFile = "";
-                var CountFiles = 0;
-                result.split('\n').forEach(line => {                
-                    if (line.substr(0, 8) == "---<EOF:") {                                        
-                        line = line
-                            .replace("---<EOF:", "")
-                            .replace(">---", "");
-                        var jsonEof = JSON.parse(line);                    
-                        
-                        const mkdirRecursive = function(dirPath, callback) {
-                           try {    
-                               var pathCreate = "";
-                                dirPath
-                                .split('/')
-                                .reduce((path, folder) => {
-                                    pathCreate += folder + '/';
-                                    if (!fs.existsSync(pathCreate)) {
-                                        fs.mkdirSync(pathCreate);
-                                    }                                                                        
-                                }, '');                      
-                                return callback()
-                            } catch (err) {
-                                if (err.code !== 'EEXIST') throw err
-                            }
-                        }
+            mu.extension = path.extname(template.mustacheFile)
+            let result = "";
+            mu.compileAndRender(mu.fileName, metadata)
+                .on('data', function (data) {
+                    result += data.toString();
+                })
+                .on("end", function () {
+                    let contextFile = "";
+                    let countFiles = 0;
+                    result.split('\n').forEach(line => {
+                        if (line.substr(0, 8) == "---<EOF:") {
+                            line = line
+                                .replace("---<EOF:", "")
+                                .replace(">---", "");
+                            let jsonEof = JSON.parse(line);
 
-                        const writeFileSync = async function (filePath) {
-                            try {                            
-                                await fs.writeFileSync(filePath, contextFile, 'utf8');
-                            } catch (err) {
-                                if (err.code !== 'EEXIST') throw err
+                            const mkdirRecursive = function (dirPath, callback) {
+                                try {
+                                    let pathCreate = "";
+                                    dirPath
+                                        .split('/')
+                                        .reduce((path, folder) => {
+                                            pathCreate += folder + '/';
+                                            if (!fs.existsSync(pathCreate)) {
+                                                fs.mkdirSync(pathCreate);
+                                            }
+                                        }, '');
+                                    return callback()
+                                } catch (err) {
+                                    if (err.code !== 'EEXIST') throw err
+                                }
                             }
-                        }
 
-                        var regex = /\\/g;
-                        var filePath = (template.outDir + "/" + jsonEof.nameFile).replace(regex, '/');                    
-                        filePath.replace("//", "/");
-                        mkdirRecursive(path.dirname(filePath), () => {
-                            writeFileSync(filePath);
-                        })                                            
-                        contextFile = "";
-                        CountFiles++;                        
-                    }
-                    else
-                    {                    
-                        contextFile += line                
-                    }
-                }); 
-                vscode.window.showInformationMessage(
-                    template.name + 
-                    " successfully generated with " + 
-                    CountFiles + " file(s) in " + template.outDir
-                );
-            });
-        });        
+                            const writeFileSync = async function (filePath) {
+                                try {
+                                    await fs.writeFileSync(filePath, contextFile, 'utf8');
+                                } catch (err) {
+                                    if (err.code !== 'EEXIST') throw err
+                                }
+                            }
+
+                            var regex = /\\/g;
+                            var filePath = (`${fileDir}/${template.outDir}/${jsonEof.nameFile}`).replace(regex, '/');
+                            filePath.replace("//", "/");
+                            mkdirRecursive(path.dirname(filePath), () => {
+                                writeFileSync(filePath);
+                            })
+                            contextFile = "";
+                            countFiles++;
+                        }
+                        else {
+                            contextFile += line
+                        }
+                    });
+                    vscode.window.showInformationMessage(
+                        template.name +
+                        " successfully generated with " +
+                        countFiles + " file(s) in " + template.outDir
+                    );
+                });
+        });
     });
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {        
+export function deactivate() {
 }
